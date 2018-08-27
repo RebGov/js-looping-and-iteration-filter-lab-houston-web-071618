edeccb00b6b90656870b40251aec688aae6ce481
RebGov <becci.govert@gmail.com>
2018-08-27 14:13:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName){
+  return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, partName) {
+  let partNameLength = partName.length;
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0,partNameLength) === partName;
+  });
+}
+ function matchName(drivers, name) {
+   return drivers.filter(function (driverName) {
+     return driverName.name.toLowerCase() === name.toLowerCase();
+   });
+ }
+
+ const drivers = [
+   {
+     name: 'Bobby',
+     hometown: 'Pittsburgh' },
+   {
+     name: 'Sammy',
+     hometown: 'New York' } ,
+   {
+     name: 'Sally',
+     hometown: 'Cleveland' },
+   {
+     name: 'Annette',
+     hometown: 'Los Angeles' },
+   {
+     name: 'Bobby',
+     hometown: 'Tampa Bay' }
+ ];
+ findMatching(drivers, 'sammy');
+
+ fuzzyMatch(drivers, 'Sa')
+ matchName(drivers, 'bobby')
